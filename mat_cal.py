6627a5828957583d899a31e6f92e9adbbe87f21f
Miracle chinaka <Miraclechinaka759@gamil.com>
2023-08-22 22:47:53 +0100

    my math cal

---
+++ mat_cal.py
@@ -0,0 +1,26 @@
+num1 = int(num1)
+num2 = int(num2)
+# Add the two numbers and store in a vairable called sum
+sum = num1 + num2
+
+# Subtract the two numbers and store in avarible called difference
+difference = num1 - num2
+
+# Multiply the two numbers and store in avarible called product
+product = num1 * num2
+
+# Divide the two numbers and store in  avarible called quotient
+quotient = num1 / num2
+
+# Find the remainder and store in a varible called difference
+remainder = num1 % num2
+
+# print out your result
+print("{}+{}={}".format(num1,num2,sum))
+print("{}-{}={}".format(num1,num2,difference))
+print("{}x{}={}".format(num1,num2,product))
+print("{}/{}={}".format(num1,num2,quotient))
+print("{}%{}={}".format(num1,num2,remainder))
+
+mat_calcullation.py                      27,0-1         Bot
+"mat_calcullation.py" 27L, 829B
