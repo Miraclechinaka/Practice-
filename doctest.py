cf84dfb7489c7022f8141d8746027bb4e7d8a0b4
Miracle chinaka <Miraclechinaka759@gamil.com>
2023-09-28 22:00:59 +0100

    my python practice

---
+++ doctest.py
@@ -0,0 +1,24 @@
+import doctest 
+...
+doctest.testmod()
+import doctest
+
+def add(a,b):
+    """
+    Given two integers,return the sum.
+
+    :param a:int
+    :param b:int
+    :return:int
+
+    >>>add (2,3)
+    5
+    """
+    return a+b
+
+doctest.testmod()
+
+
+
+
+
